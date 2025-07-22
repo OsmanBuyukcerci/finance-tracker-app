da19b730488b4fa44770dc765df91bc50c1a7337
Osman <osmanbuyukcerci17@gmail.com>
2025-07-22 11:52:54 +0300

    refactor: property names

--- backend/finance-tracker-app/Middlewares/RequestLoggingMiddleware.cs
+++ backend/finance-tracker-app/Middlewares/RequestLoggingMiddleware.cs
@@ -2,23 +2,23 @@
 {
     public class RequestLoggingMiddleware
     {
-        private readonly RequestDelegate _next;
-        private readonly ILogger<RequestLoggingMiddleware> _logger;
+        private readonly RequestDelegate _Next;
+        private readonly ILogger<RequestLoggingMiddleware> _Logger;
 
         public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
         {
-            _next = next;
-            _logger = logger;
+            _Next = next;
+            _Logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             // Log the request details
-            _logger.LogInformation("Handling request: {Method} {Path}", context.Request.Method, context.Request.Path);
+            _Logger.LogInformation("Handling request: {Method} {Path}", context.Request.Method, context.Request.Path);
             // Call the next middleware in the pipeline
-            await _next(context);
+            await _Next(context);
             // Log the response details
-            _logger.LogInformation("Finished handling request. Response status code: {StatusCode}", context.Response.StatusCode);
+            _Logger.LogInformation("Finished handling request. Response status code: {StatusCode}", context.Response.StatusCode);
         }
     }
 }
