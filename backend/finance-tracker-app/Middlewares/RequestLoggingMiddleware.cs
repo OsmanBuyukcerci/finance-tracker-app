3856f60a0fe2b83582ece08e5d2d6cb2856a80fc
Osman <osmanbuyukcerci17@gmail.com>
2025-07-22 11:47:28 +0300

    feat: add logging middleware

---
+++ backend/finance-tracker-app/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace finance_tracker_app.Middlewares
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Log the request details
+            _logger.LogInformation("Handling request: {Method} {Path}", context.Request.Method, context.Request.Path);
+            // Call the next middleware in the pipeline
+            await _next(context);
+            // Log the response details
+            _logger.LogInformation("Finished handling request. Response status code: {StatusCode}", context.Response.StatusCode);
+        }
+    }
+}
--- backend/finance-tracker-app/Program.cs
+++ backend/finance-tracker-app/Program.cs
@@ -1,4 +1,5 @@
 using finance_tracker_app.Data;
+using finance_tracker_app.Middlewares;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+app.UseMiddleware<RequestLoggingMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
 
